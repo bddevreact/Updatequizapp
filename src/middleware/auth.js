168bd4550b55ab2266b19d4e053c2ace940b0e51
bddevreact <cristianou951@gmail.com>
2025-10-20 03:52:09 +0600

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,3 +3,5 @@ const { authenticate, authenticateAdmin } = require('../utils/auth');
 // Export the auth middleware functions
 module.exports = authenticate;
 module.exports.admin = authenticateAdmin;
+
+
